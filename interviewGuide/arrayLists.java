1644643d7670cc15c422f9390ac2e8a8a80d1e4f
Subba <kvsubbareddy238@gmail.com>
2026-01-05 21:32:29 +0530

    interview guide

---
+++ interviewGuide/arrayLists.java
@@ -0,0 +1,16 @@
+package interviewGuide;
+
+import java.util.ArrayList;
+
+
+public class arrayLists {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add( "one");
+        list.add("two");
+        list.add("three");
+        System.out.println(list);
+
+    }
+
+    }
